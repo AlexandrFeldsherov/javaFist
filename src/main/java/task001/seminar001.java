e83f318260712c8fba6a9b993bb21646c5c04fb1
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-04-23 15:31:26 +0300

    init commit

---
+++ src/main/java/task001/seminar001.java
@@ -0,0 +1,9 @@
+package task001;
+
+public class seminar001 {
+    public static void main(String[] args) {
+        System.out.println("123");
+
+    }
+}
+
---
+++ src/main/java/task002/Task002.java
@@ -0,0 +1,7 @@
+package task002;
+
+public class Task002 {
+    public static void main(String[] args) {
+
+    }
+}
